c45a49edc4730612dc0afd8b083f31b6a435d0ca
mickey <59575072+Spoorloos@users.noreply.github.com>
2023-03-05 20:25:53 +0100

    Update source.lua

--- library/sense/source.lua
+++ library/sense/source.lua
@@ -189,7 +189,7 @@ function EspObject:Update()
 
 	self.options = interface.teamSettings[interface.isFriendly(self.player) and "friendly" or "enemy"];
 	self.character = interface.getCharacter(self.player);
-	self.health, self.maxHealth = interface.getHealth(self.character);
+	self.health, self.maxHealth = interface.getHealth(self.player);
 	self.weapon = interface.getWeapon(self.player);
 	self.enabled = self.options.enabled and self.character and not
 		(#interface.whitelist > 0 and not find(interface.whitelist, self.player.UserId));
@@ -700,7 +700,8 @@ function EspInterface.getCharacter(player)
 	return player.Character;
 end
 
-function EspInterface.getHealth(character)
+function EspInterface.getHealth(player)
+	local character = player and EspInterface.getCharacter(player);
 	local humanoid = character and findFirstChildOfClass(character, "Humanoid");
 	if humanoid then
 		return humanoid.Health, humanoid.MaxHealth;
